70bb1519e6db79b1de4cc7dd2cf2be288029f63a
Martin Zlámal <mrtnzlml@gmail.com>
2015-02-22 15:46:01 +0100

    TestGenerator fix --- not ready yet

--- src/TestGenerator.php
+++ src/TestGenerator.php
@@ -1,15 +1,15 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../../../autoload.php'; //FIXME: not good
 
 //TODO: upozornění na přemazání již napsaných testů
 //TODO: lepší generování podle situace
 //TODO: 2 parametry: source a destination
 
 $configurator = new Nette\Configurator;
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->setTempDirectory(__DIR__ . '/../../../../temp'); //FIXME: not good
 $loader = $configurator->createRobotLoader()
-	->addDirectory(__DIR__ . '/../tests/')
+	->addDirectory(__DIR__ . '/../../../../app/')//FIXME: not good
 	->register();
 
 foreach ($loader->getIndexedClasses() as $class => $filename) {
@@ -18,8 +18,9 @@ foreach ($loader->getIndexedClasses() as $class => $filename) {
 		$class = new Nette\PhpGenerator\ClassType($refl->getShortName() . 'Test');
 		$class->addDocument('@testCase')->setExtends('Tester\TestCase');
 		$class->addProperty('tester')->setVisibility('private');
+		$presName = preg_replace('/Presenter$/i', '', $refl->getShortName());
 		$class->addMethod('__construct')
-			->setBody('$this->tester = new PresenterTester($container, \'' . $refl->getShortName() . '\');')
+			->setBody('$this->tester = new PresenterTester($container, \'' . $presName . '\');')
 			->addParameter('container')
 			->setTypeHint('Nette\DI\Container');
 		/** @var ReflectionMethod $method */
@@ -36,14 +37,20 @@ foreach ($loader->getIndexedClasses() as $class => $filename) {
 			}
 		}
 		$output = "<?php\n\nnamespace Test;\n\nuse Nette;\nuse Tester;\n\n";
-		//TODO: require container
+		$output .= '$container = require __DIR__ . \'/../bootstrap.php\';' . "\n\n";
 		$output .= (string)$class;
+		$output .= "\n" . '$test = new ' . $refl->getShortName() . 'Test($container);';
+		$output .= "\n\n" . '$test->run();' . "\n";
 		if ($refl->inNamespace()) {
-			@mkdir($refl->getNamespaceName());
-			file_put_contents($refl->getNamespaceName() . DIRECTORY_SEPARATOR . $refl->getShortName() . 'Test.phpt', (string)$output);
+			$dir = preg_replace('/\\\/', '', $refl->getNamespaceName());
+			@mkdir(__DIR__ . '/../../../../tests/' . $dir);
+			file_put_contents(__DIR__ . '/../../../../tests/' . $dir . DIRECTORY_SEPARATOR . $refl->getShortName() . 'Test.phpt', (string)$output);
 		} else {
+			//FIXME: not good: paths
 			@mkdir('presenters');
-			file_put_contents('presenters' . DIRECTORY_SEPARATOR . $refl->getShortName() . 'Test.phpt', (string)$output);
+			file_put_contents(__DIR__ . '/../../../../tests/' . $dir . 'presenters' . DIRECTORY_SEPARATOR . $refl->getShortName() . 'Test.phpt', (string)$output);
 		}
 	}
 }
+
+die("OK");
