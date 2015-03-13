f87ecf0d7ab6d8b2d679a6e7728c879f47018566
Martin Zlámal <mrtnzlml@gmail.com>
2015-03-13 12:26:20 +0100

    added bootstrap class

---
+++ src/Bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Test;
+
+use Nette;
+use Tester;
+use Tracy;
+
+/**
+ * Class Bootstrap
+ * @package Test
+ */
+class Bootstrap extends Nette\Object {
+
+	public static function setup($root_dir) {
+		if (!class_exists('Tester\Assert')) {
+			echo "Install Nette Tester using `composer update --dev`\n";
+			exit(1);
+		}
+		umask(0);
+		Tester\Environment::setup();
+		date_default_timezone_set('Europe/Prague');
+		define('TEMP_DIR', $root_dir . '/temp/');
+		//Tester\Helpers::purge(TEMP_DIR);
+		@chmod(TEMP_DIR, 0777);
+		Tracy\Debugger::$logDirectory = TEMP_DIR;
+		$_ENV = $_GET = $_POST = $_FILES = array();
+		$configurator = new Nette\Configurator;
+		$configurator->setTempDirectory(TEMP_DIR);
+		return $configurator;
+	}
+
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,11 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/Bootstrap.php';
 require __DIR__ . '/presenters/PresenterPresenter.php';
 require __DIR__ . '/ModuleModule/presenters/PresenterPresenter.php';
 
-if (!class_exists('Tester\Assert')) {
-	echo "Install Nette Tester using `composer update --dev`\n";
-	exit(1);
-}
-
-Tester\Environment::setup();
-date_default_timezone_set('Europe/Prague');
-
-@mkdir(__DIR__ . '/../temp');
-
-$configurator = new Nette\Configurator;
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator = Test\Bootstrap::setup(__DIR__);
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__ . '/../src')
 	->register();
