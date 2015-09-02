62c9ada39a861a7e515575723cc70f6e57e3c835
Martin Zlámal <mrtnzlml@gmail.com>
2015-09-02 09:14:45 +0200

    Tests: delete temp directory before tests start (closes #3)

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
+RMDIR /s /q ./temp
 SET BIN_TARGET=%~dp0/../vendor/nette/tester/Tester/tester.php
 php "%BIN_TARGET%" -c php.ini --coverage-src ./../src/ --coverage ./coverage.html %* ./../tests
