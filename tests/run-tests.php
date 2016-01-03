53e8c7861ca9c09cd396a49d84ab0de664f8a319
Martin Zlámal <mrtnzlml@gmail.com>
2016-01-03 18:17:41 +0100

    Update tests (Linux only)

---
+++ tests/run-tests.php
@@ -0,0 +1,34 @@
+<?php
+
+$position = $_SERVER['argc'] - 1;
+
+if ($position === 0) {
+	$_SERVER['argv'][++$position] = '.';
+}
+$_SERVER['argv'][++$position] = '-c';
+
+$os = stristr(PHP_OS, 'LINUX') ? 'unix' : 'win';
+$_SERVER['argv'][++$position] = "php-$os.ini";
+
+// $_SERVER['argv'][++$position] = '--coverage';
+// $_SERVER['argv'][++$position] = 'coverage.html';
+// $_SERVER['argv'][++$position] = '--coverage-src';
+// $_SERVER['argv'][++$position] = './../app';
+
+$dir = __DIR__ . '/cache';
+
+if (!is_dir($dir)) {
+	mkdir($dir);
+}
+
+$rdi = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+$rii = new \RecursiveIteratorIterator($rdi, \RecursiveIteratorIterator::CHILD_FIRST);
+foreach ($rii as $entry) {
+	if ($entry->isDir()) {
+		rmdir($entry);
+	} else {
+		unlink($entry);
+	}
+}
+
+require __DIR__ . '/../vendor/bin/tester';
